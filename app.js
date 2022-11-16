f77e1051d5db37c5c792ef8d0c042a6414c371db
vcristobal3@gmail.com <vcristobal3@gmail.com>
2022-11-16 13:09:04 -0500

    corsAdd

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 import * as dotenv from 'dotenv' 
 dotenv.config()
-import Cors from 'cors'
-
-app.use(Cors)
 
 import {ServidorAPI} from './API/ServidorAPI.js'
 import { application } from 'express'

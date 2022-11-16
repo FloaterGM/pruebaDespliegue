2cabf34e0af1218abb28af61d18b83e9627be979
vcristobal3@gmail.com <vcristobal3@gmail.com>
2022-11-16 08:02:49 -0500

    Cors

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 import * as dotenv from 'dotenv' 
 dotenv.config()
+import Cors from 'cors'
+
+app.use(Cors)
 
 import {ServidorAPI} from './API/ServidorAPI.js'
+import { application } from 'express'
 
 let servidorHoteles=new ServidorAPI() //Intancia de una clase (OBJETO)
 servidorHoteles.despertarServidor()
